ca9b9779d9e1a0688344f3f8b7746870f3447400
Diego Olivar <diegoolivarsantiago@gmail.com>
2026-07-21 13:22:44 -0500

    v3.0.0

---
+++ script-tablero.js
@@ -0,0 +1,58 @@
+// Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyDR-P_S7CAGCEw8WlJzTgHPkDMPIOdZWQk",
+authDomain: "ilovejuan.firebaseapp.com",
+databaseURL: "https://ilovejuan-default-rtdb.firebaseio.com",
+projectId: "ilovejuan",
+storageBucket: "ilovejuan.firebasestorage.app",
+messagingSenderId: "57213839627",
+appId: "1:57213839627:web:429c76d2183f2c924d1a0e",
+measurementId: "G-97C9T9N0XD"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Cuadrícula
+const grid = document.getElementById('grid-numeros');
+for (let i = 1; i <= 50; i++) {
+const divDiv = document.createElement('div');
+divDiv.className = 'numero';
+divDiv.id = `num-${i}`;
+divDiv.textContent = i;
+grid.appendChild(divDiv);
+}
+
+// Cambios en tiempo real
+const nombresRef = ref(db, 'sorteo/nombres');
+
+onValue(nombresRef, (snapshot) => {
+const datos = snapshot.val() || {};
+
+// Recorrido de tablero
+for (let i = 1; i <= 50; i++) {
+    const elementoNumero = document.getElementById(`num-${i}`);
+    
+    if (datos[i] && datos[i].trim() !== "") {
+    elementoNumero.classList.add('registrado');
+    elementoNumero.title = `Registrado por: ${datos[i]}`; // Monstrar nombre
+    } else {
+    elementoNumero.classList.remove('registrado');
+    elementoNumero.title = "";
+    }
+}
+});
+
+// Sincronizar imagen
+const imagenRef = ref(db, 'sorteo/imagen');
+onValue(imagenRef, (snapshot) => {
+const imgVal = snapshot.val();
+if (imgVal) {
+    document.body.style.backgroundImage = `url('${imgVal}')`;
+} else {
+    document.body.style.backgroundImage = "url('plantilla-sorteo.png')";
+}
+});
